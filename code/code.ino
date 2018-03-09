75ae48af0570abdebbc5be2d95f181b92099efb5
aubedac <adrianubedaportugues@gmail.com>
2018-03-09 10:32:57 +0100

    Alpha 0.3

--- code/code.ino
+++ code/code.ino
@@ -3,84 +3,98 @@
 // Estos son los pines a la Arduino donde la pantalla esta conectada
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-const int UltraSPinEntrada = 6;
-const int UltraSPinSalida = 7;
+const int PIRPin = 6;
+const int PIRPinFuera = 9;
 const int hallPin = 8;
 const int ledPin = 13;
-int photocellPin = 0;
-int photocellReading = -1;
-int tempReading = -1;
-int tempPin = 1 ;
-int PIREntrada = -1;
-int PIRSalida = -1;
 
+int PIROut = -1;
+int PIRIn = -1;
+
+int pirState = LOW;
+int pirStateFuera = LOW;
+int val = 0;
+
+int aux = 13;
 int control = 0;
-int maxNum = 10;
-int x = 0;
 int valuePIR = -1;
 int valueHALL = -1;
-bool flag = false;
-
-float voltTemp = 0.0;
-float temp = 0.0;
 
 void setup() {
   // Columnas y filas de la pantalla LCD:
   lcd.begin(16, 2);
-  Serial.begin(9600);
-  pinMode(UltraSPinEntrada, OUTPUT);
-  pinMode(UltraSPinSalida, INPUT);
+  pinMode(PIRPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  //pinMode(hallPin, INPUT);
+  pinMode(hallPin, INPUT);
+  Serial.begin(9600);
 }
 
- void loop()
-        {   long duracion, distancia ;
-       pinMode(UltraSPinEntrada, OUTPUT);
-            digitalWrite(UltraSPinEntrada, LOW);
-            delayMicroseconds(2);
-            digitalWrite(UltraSPinEntrada, HIGH);
-            delayMicroseconds(5);
-            pinMode(UltraSPinEntrada, INPUT);
-            digitalWrite(UltraSPinEntrada, LOW);
-            duracion = pulseIn(UltraSPinEntrada, HIGH) ;
-            distancia = duracion / 2 / 29.1  ;
-            Serial.println(String(distancia) + " cm.") ;
-            int Limite = 200 ;                  // Medida en vacío del sensor
-            if ( distancia < Limite)
-                 Serial.println('Loquequieras');
-            else
-                 Serial.println('Esto es una traza');
-            delay (100);                  // Para limitar el número de mediciones
+void loop() {
+   lcd.setCursor(0,0);
+   lcd.print("People: ");
+   valueHALL = digitalRead(hallPin);
+   if (valueHALL == LOW){
+      //Cerrado
+      PIRIn = PIRReading(PIRPin);
+      if (digitalRead(hallPIN == HIGH) && PIRIn == HIGH{
+        PIROut = PIRReading(PIRPinFuera);
+        if (PIROut == HIGH){
+          control -= 1;
         }
+      }
+   }else{
+      //Abierto
+   }
+   //control = testPIR(control);
+   //prettyPrint(control, aux);
+   //Serial.print(control);
+   //delay(100);*/
+   hallSensor();
+}
 
-
-/*void loop() {
-   PIREntrada = pulseIn(echoPin, HIGH) ;
-
-  //PIREntrada = PIRSensor(UltraSPinEntrada);
-  Serial.println(PIREntrada);
-  PIRSalida = PIRSensor(UltraSPinSalida);
-  if (PIREntrada && !PIRSalida){
-    PIRSalida = PIRSensor(UltraSPinSalida);
-    if (PIRSalida){
-      Serial.println("Entrando");
-    }else{
-      Serial.println("Algo raruno entrando");
+int PIRReading(int PIRPins) {
+  val = digitalRead(PIRPins);  // read input value
+  
+  if (val == HIGH) {            // check if the input is HIGH
+    //lcd.clear();
+    if (pirState == LOW) {
+      digitalWrite(ledPin, HIGH);
+      // we have just turned on
+      //lcd.print("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
     }
-  }else if (!PIREntrada && PIRSalida){
-    PIREntrada = PIRSensor(UltraSPinSalida);
-    if (PIREntrada){
-      Serial.println("Saliendo");
-    }else{
-      Serial.println("Algo raruno saliendo");
+  } else {
+    Serial.print("--->");
+    digitalWrite(ledPin, LOW);
+    if (pirState == HIGH){
+      // we have just turned of
+      //lcd.print("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
     }
   }
-  PIREntrada = -1;
-  PIRSalida = -1;
-}*/
+  //delay(100);
+  return pirState;
+}
 
-void testScreen(){
+void prettyPrint(int control, int aux){
+  for(aux++; aux <= 15; aux++){
+    lcd.setCursor(aux,1);
+    lcd.print(" ");
+  }
+  if (control < 10){
+    lcd.setCursor(15,1);
+  }else if (control >= 10 && control <= 99){
+    lcd.setCursor(14, 1);
+  }else{
+    lcd.setCursor(13, 1);
+  }
+  lcd.print(control);
+  aux = 13;
+}
+
+/*void testScreen(){
  control = 0;
  // Mensaje:
   lcd.print("Contador cutre:");
@@ -113,15 +127,29 @@ void testScreen(){
   flag = false;
   delay(1000);
   lcd.clear(); 
-}
+}*/
 
-void hallSensor(){
-  if(valueHALL == LOW){
-    lcd.print("CERRADO");
+/*void hallSensor(){
+  valueHALL = digitalRead(hallPin);
+  
+  /*if(valueHALL == LOW){
+    // CERRADO
+    digitalWrite(ledPin, LOW);
+    if (pirState == HIGH){
+      // we have just turned of
+      //lcd.print("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
   }else{
-    lcd.print("ABIERTO");
+    // ABIERTO
+    pirState = HIGH
+   //control = testPIR(control);
+   //prettyPrint(control, aux);
+   //Serial.print(control);
   }
-}
+  //delay(2000);
+}*/
 
 int PIRSensor(int PIRPin){
   valuePIR = digitalRead(PIRPin);
@@ -133,23 +161,5 @@ int PIRSensor(int PIRPin){
     lcd.print("N");
   }
   return valuePIR;
-}
-
-void photoSensor(int photocellPin, int ledPin){
-  int reading = -1;
-  reading = analogRead(photocellPin);
-  digitalWrite(ledPin, HIGH);
-  delay(reading);
-  digitalWrite(ledPin, LOW); 
-  delay(reading);
-  Serial.print("LDR: ");
-  Serial.println(reading);
-}
-
-int tempSensor(int tempPin){
-  tempReading = analogRead(tempPin);
-  voltTemp = 5.0 / 1024 * tempReading;
-  temp = voltTemp * 100 - 50;
-  return temp;
 }
  
